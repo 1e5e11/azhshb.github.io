e8b79c3e509d988fa9f8f5cd8e53cb40fa08b7d2
1e5e11 <108060168+1e5e11@users.noreply.github.com>
2022-11-03 21:15:01 +0800

    Update AAAAA.js

--- q/p/AAAAA.js
+++ q/p/AAAAA.js
@@ -2,3 +2,7 @@ const serverperform=``
 function AAAAA(){
     alert('5A!')
 }
+function tianjianeirong(){
+var f=document.getElementById('zbx').innerHTML
+document.getElementById('zbx').innerHTML=f+`<div style="position: absolute;height: 150px;width: 300px;right: 0px;bottom: 0px;background-image: url(t.png);background-size:cover;background-position: center;z-index:-1;"></div>`
+}
