6f08a49ce9dc73532c21280933956f27b2cf5e61
1e5e11 <108060168+1e5e11@users.noreply.github.com>
2023-08-03 17:13:19 +0800

    Add files via upload

--- q/p/AAAAA.js
+++ q/p/AAAAA.js
@@ -2,7 +2,17 @@ const serverperform=``
 function AAAAA(){
     alert('5A!')
 }
-function tianjianeirong(){
+var beijinliebiao=[
+    ['dongtian1.2.png','150','300'],
+    ['qiutian3.5.png','120','200'],
+    ['wanshengjie2.1.png','300','150'],
+    ['','','']
+]
+function grdthjfyjtgjitgyj(name,k,g){
 var f=document.getElementById('elseXS').innerHTML
-document.getElementById('elseXS').innerHTML=f+`<div style="position: absolute;height: 300px;width: 150px;right: 0px;bottom: 0px;background-image: url(http://h.ssssssss.co/q/p/t.png);background-size:cover;background-position: center;z-index:-1;"></div>`
+document.getElementById('elseXS').innerHTML=f+`<div style="position: absolute;height: `+g+`px;width: `+k+`px;right: 0px;bottom: 0px;background-image: url(http://h.ssssssss.co/q/p/`+name+`);background-size:cover;background-position: center;z-index:-1;"></div>`
+}
+function tianjianeirong(){
+    var t=1
+    grdthjfyjtgjitgyj(beijinliebiao[t][0],beijinliebiao[t][1],beijinliebiao[t][2])
 }
