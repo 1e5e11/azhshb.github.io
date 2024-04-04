49325672bbd212d143557b22b7f0bebd7d7ff102
1e5e11 <108060168+1e5e11@users.noreply.github.com>
2024-04-04 12:17:14 +0800

    Add files via upload

--- q/p/AAAAA.js
+++ q/p/AAAAA.js
@@ -6,13 +6,13 @@ var beijinliebiao=[
     ['dongtian1.2.png','150','300'],
     ['qiutian3.5.png','120','200'],
     ['wanshengjie2.1.png','300','150'],
-    ['','','']
+    ['muxing1.1.png','150','150']
 ]
 function grdthjfyjtgjitgyj(name,k,g){
 var f=document.getElementById('elseXS').innerHTML
 document.getElementById('elseXS').innerHTML=f+`<div style="position: absolute;height: `+g+`px;width: `+k+`px;right: 0px;bottom: 0px;background-image: url(http://h.ssssssss.co/q/p/`+name+`);background-size:cover;background-position: center;z-index:-1;"></div>`
 }
 function tianjianeirong(){
-    var t=0
+    var t=3
     grdthjfyjtgjitgyj(beijinliebiao[t][0],beijinliebiao[t][1],beijinliebiao[t][2])
 }
